d44408878e340556ec94301ebf98595a18ffc549
adefirmanf_98 <ade.fauzi@payfazz.com>
2019-06-12 17:35:17 +0700

    Add some unit test

--- test/unWrapMsg.js
+++ test/unWrapMsg.js
@@ -3,17 +3,56 @@ const expect = require('chai').expect
 
 describe("ISO8583 Unwrap message", ()=>{
 
-  const X = new ISO8583({
-    header : 'ISO015000017',
-    mti : true
+  it("Should return valid bitmap with element name set to true", ()=>{
+    const X = new ISO8583({
+      elementName : true,
+      header : 'ISO015000017',
+      mti : true
+    })
+
+    const UnWrapMsg = X.unWrapMsg('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
+    expect(UnWrapMsg.get('SECONDARY_BITMAP')).to.equal('0000004000000000')
+    expect(UnWrapMsg.get('PRIMARY_ACCOUNT_NUMBER')).to.equal('160000912478128124')
+    expect(UnWrapMsg.get('PROCESSING_CODE')).to.equal('561999')
+    expect(UnWrapMsg.get('AMOUNT_TRANSACTION')).to.equal('000000025000')
+    expect(UnWrapMsg.get('TRANSACTION_DATETIME')).to.equal('0323163555') 
   })
 
-  it("Should return valid bitmap", ()=>{
+  it("Should return valid bitmap with element name set to false", ()=>{
+    const X = new ISO8583({
+      elementName : true,
+      header : 'ISO015000017',
+      mti : true
+    })
+    
     const UnWrapMsg = X.unWrapMsg('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
     expect(UnWrapMsg.get(1)).to.equal('0000004000000000')
     expect(UnWrapMsg.get(2)).to.equal('160000912478128124')
     expect(UnWrapMsg.get(3)).to.equal('561999')
     expect(UnWrapMsg.get(4)).to.equal('000000025000')
     expect(UnWrapMsg.get(7)).to.equal('0323163555') 
-  }) 
+  })
+
+  it("Should return valid bitmap with initialization", ()=>{
+
+    const X = new ISO8583({
+      header : 'ISO015000017',
+      mti : true
+    })
+
+    X.init([
+      ['SECONDARY', {bitmap : 1, length : 16}],
+      ['ACCOUNT', {bitmap : 2, length : 18}],
+      ['PROCESSING', {bitmap : 3, length : 6}],
+      ['AMOUNT', {bitmap : 4, length : 12}],
+      ['DATETIME', {bitmap : 7, length : 10}]
+    ])
+
+    const UnWrapMsg = X.unWrapMsg('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
+    expect(UnWrapMsg.get('SECONDARY')).to.equal('0000004000000000')
+    expect(UnWrapMsg.get('ACCOUNT')).to.equal('160000912478128124')
+    expect(UnWrapMsg.get('PROCESSING')).to.equal('561999')
+    expect(UnWrapMsg.get('AMOUNT')).to.equal('000000025000')
+    expect(UnWrapMsg.get('DATETIME')).to.equal('0323163555') 
+  })
 })
--- test/wrapMsg.js
+++ test/wrapMsg.js
@@ -2,26 +2,58 @@ const ISO8583 = require('..')
 const expect = require('chai').expect
 
 describe("ISO8583 Wrap message", ()=>{
-
-  const X = new ISO8583({
-    header : 'ISO015000017',
-    mti : true
-  })
-
-  X.init([
-    ['SECONDARY_BITMAP', {bitmap : 1, length : 16}],
-    ['PRIMARY_ACCOUNT_NUMBER', {bitmap : 2, length : 18}],
-    ['PROCESSING_CODE', {bitmap : 3, length : 6}],
-    ['TRANSACTION_AMOUNT', {bitmap : 4, length : 12}],
-    ['DATE_TIME', {bitmap : 7, length : 10}]
-  ])
-
-  it("Should return valid message", ()=>{
+  it("Should return valid message with element name set to true", ()=>{
+    const X = new ISO8583({
+      elementName : true,
+      header : 'ISO015000017',
+      mti : true
+    })
+  
     X.set('SECONDARY_BITMAP', '0000004000000000')
     X.set('PRIMARY_ACCOUNT_NUMBER', '160000912478128124')
     X.set('PROCESSING_CODE', '561999')
-    X.set('TRANSACTION_AMOUNT', '25000')
-    X.set('DATE_TIME', '0323163555')
+    X.set('AMOUNT_TRANSACTION', '25000')
+    X.set('TRANSACTION_DATETIME', '0323163555')
+
+    expect(X.wrapMsg('0800')).to.equal('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
+
+  })
+
+  it("Should return valid message with element name set to false", ()=>{
+    const X = new ISO8583({
+      elementName : false,
+      header : 'ISO015000017',
+      mti : true
+    })
+  
+    X.set(1, '0000004000000000')
+    X.set(2, '160000912478128124')
+    X.set(3, '561999')
+    X.set(4, '25000')
+    X.set(7, '0323163555')
+
+    expect(X.wrapMsg('0800')).to.equal('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
+
+  })
+
+  it("Should return valid message with initialization", ()=>{
+    const X = new ISO8583({
+      header : 'ISO015000017',
+      mti : true
+    })
+    X.init([
+      ['SECONDARY', {bitmap : 1, length : 16}],
+      ['ACCOUNT', {bitmap : 2, length : 18}],
+      ['PROCESSING', {bitmap : 3, length : 6}],
+      ['AMOUNT', {bitmap : 4, length : 12}],
+      ['DATETIME', {bitmap : 7, length : 10}]
+    ])
+    
+    X.set('SECONDARY', '0000004000000000')
+    X.set('ACCOUNT', '160000912478128124')
+    X.set('PROCESSING', '561999')
+    X.set('AMOUNT', '25000')
+    X.set('DATETIME', '0323163555')
 
     expect(X.wrapMsg('0800')).to.equal('ISO0150000170800f20000000000000000000040000000001600009124781281245619990000000250000323163555')
 
